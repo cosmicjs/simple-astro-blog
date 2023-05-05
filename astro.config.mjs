05d43f3e740f71b8fe91d02f693261401d99d08f
stefkudla <stefankudla@gmail.com>
2023-05-05 14:55:28 -0400

    Full TS, Eslint, Prettier support / package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import robotsTxt from 'astro-robots-txt'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image(), robotsTxt()],
-  site: "https://simple-astro-blog.vercel.app",
+  integrations: [
+    tailwind(),
+    react(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+    robotsTxt()
+  ],
+  site: 'https://simple-astro-blog.vercel.app',
   experimental: {
-    integrations: true,
-  },
-});
+    integrations: true
+  }
+})
--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,8 +1,9 @@
-import ReactMarkdown from "react-markdown";
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const components: {} = {
   p: (p: { children: string }) => {
-    return <p className="my-6 text-lg">{p.children}</p>;
+    return <p className="my-6 text-lg">{p.children}</p>
   },
   a: (a: { children: string; href: string }) => {
     return (
@@ -12,13 +13,13 @@ const components: {} = {
         className="text-blue-600 border-b border-b-transparent hover:border-b-zinc-300 transition-colors border-">
         {a.children}
       </a>
-    );
+    )
   },
   h2: (h2: { children: string }) => {
-    return <h2 className="text-3xl">{h2.children}</h2>;
+    return <h2 className="text-3xl">{h2.children}</h2>
   },
   h3: (h3: { children: string }) => {
-    return <h3 className="text-2xl">{h3.children}</h3>;
+    return <h3 className="text-2xl">{h3.children}</h3>
   },
   img: (img: { src: string; alt: string }) => {
     return (
@@ -29,15 +30,17 @@ const components: {} = {
         decoding="async"
         className="rounded-md shadow"
       />
-    );
-  },
-};
+    )
+  }
+}
 
 const PostBody: React.FC<{ content: string }> = ({ content }) => {
   return (
-    <ReactMarkdown components={components} className="flex flex-col gap-y-4">
+    <ReactMarkdown
+      components={components}
+      className="flex flex-col gap-y-4 py-12">
       {content}
     </ReactMarkdown>
-  );
-};
-export default PostBody;
+  )
+}
+export default PostBody
