bcb260898a1f24da44d64dafebb9b851d2cb6376
Stefan Kudla <stefankudla@gmail.com>
2023-05-05 18:20:56 -0400

    Fixes and Improvements (Null checks, Typescript support, Eslint, Prettier) (#8)
    * Fix null checks / add full TS support
    
    * Full TS, Eslint, Prettier support / package updates
    
    * Update Cosmic Logo 2023
    
    * Install prettier tailwindcss plugin
    
    * Full Lint script - Eslint / prettier
    
    * Fix Vercel deployment link
    
    * remove dark: css class
    
    * remove console log

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -1,24 +1,25 @@
-import ReactMarkdown from "react-markdown";
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const components: {} = {
   p: (p: { children: string }) => {
-    return <p className="my-6 text-lg">{p.children}</p>;
+    return <p className="my-6 text-lg">{p.children}</p>
   },
   a: (a: { children: string; href: string }) => {
     return (
       <a
         href={a.href}
         target="_blank"
-        className="text-blue-600 border-b border-b-transparent hover:border-b-zinc-300 transition-colors border-">
+        className="border- border-b border-b-transparent text-blue-600 transition-colors hover:border-b-zinc-300">
         {a.children}
       </a>
-    );
+    )
   },
   h2: (h2: { children: string }) => {
-    return <h2 className="text-3xl">{h2.children}</h2>;
+    return <h2 className="text-3xl">{h2.children}</h2>
   },
   h3: (h3: { children: string }) => {
-    return <h3 className="text-2xl">{h3.children}</h3>;
+    return <h3 className="text-2xl">{h3.children}</h3>
   },
   img: (img: { src: string; alt: string }) => {
     return (
@@ -29,15 +30,17 @@ const components: {} = {
         decoding="async"
         className="rounded-md shadow"
       />
-    );
-  },
-};
+    )
+  }
+}
 
 const PostBody: React.FC<{ content: string }> = ({ content }) => {
   return (
-    <ReactMarkdown components={components} className="flex flex-col gap-y-4">
+    <ReactMarkdown
+      components={components}
+      className="flex flex-col gap-y-4 py-12">
       {content}
     </ReactMarkdown>
-  );
-};
-export default PostBody;
+  )
+}
+export default PostBody
